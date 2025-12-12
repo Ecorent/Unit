93da23d267b5d90afab145044f565c59d87ee5d3
Emmanuel <emmirongolo0804@gmail.com>
2025-12-12 16:12:47 -0500

    rf45

--- js/home_page.js
+++ js/home_page.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
+const navbar = document.querySelector(".navbar");
 
 hamburger.addEventListener('click', () => {
   mobileMenu.classList.toggle('open');
+  navbar.classList.toggle("menu-open");
 });
