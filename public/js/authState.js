4b88c3990e4100716052d24d8baf832a21e4147d
Emmanuel <emmirongolo0804@gmail.com>
2025-12-23 17:11:01 -0500

    kjhgfds

--- public/js/authState.js
+++ public/js/authState.js
@@ -1,18 +1,19 @@
 // public/js/authState.js
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-const loginLink = document.getElementById("loginLink");
-const profileIcon = document.getElementById("profileIcon");
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
+  const loginLink = document.getElementById("loginLink");
+  const profileIcon = document.getElementById("profileIcon");
+
+  if (!loginLink || !profileIcon) return;
+
   if (user) {
-    // User logged in
-    if (loginLink) loginLink.classList.add("hidden");
-    if (profileIcon) profileIcon.classList.remove("hidden");
+    loginLink.classList.add("hidden");
+    profileIcon.classList.remove("hidden");
   } else {
-    // User logged out
-    if (loginLink) loginLink.classList.remove("hidden");
-    if (profileIcon) profileIcon.classList.add("hidden");
+    loginLink.classList.remove("hidden");
+    profileIcon.classList.add("hidden");
   }
 });
