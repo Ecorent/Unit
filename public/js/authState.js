97b9a73aaccf120e49c6eacdd457372c3498b1ac
Emmanuel <emmirongolo0804@gmail.com>
2026-01-09 08:44:19 -0500

    jyhrgfeds

--- public/js/authState.js
+++ public/js/authState.js
@@ -44,14 +44,7 @@ onAuthStateChanged(auth, (user) => {
 
     const redirectToLogin = (e) => {
       e.stopPropagation();
-
-      const isDesktop = window.matchMedia("(min-width: 769px)").matches;
-
-      if (isDesktop) {
-        window.location.href = "/login.html";
-      } else {
-        window.location.replace("/login.html");
-      }
+      window.location.href = "/login.html";
     };
 
     profileToggle.onclick = redirectToLogin;
