86b375a5de03bce1941b8a67150da8237c65022e
Emmanuel <emmirongolo0804@gmail.com>
2026-01-10 10:12:08 -0500

    ytetsrce

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,14 +2,10 @@
 import { translations } from "/js/i18n.js";
 const navbarContainer = document.getElementById("navbar");
 
-/* Reserve navbar space immediately */
-navbarContainer.classList.add("navbar-skeleton");
-
 fetch("/partials/navbar.html")
   .then(res => res.text())
   .then(html => {
     navbarContainer.innerHTML = html;
-    navbarContainer.classList.remove("navbar-skeleton");
 
     /* Load auth state logic AFTER navbar exists */
     const authScript = document.createElement("script");
